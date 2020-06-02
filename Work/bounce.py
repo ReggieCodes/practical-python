33ed88dd80d2a5625834157a2f376ca5a81ca896
ReggieCodes <54084392+ReggieCodes@users.noreply.github.com>
2020-06-01 22:16:23 -0500

    Exercise 1.5 complete.

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100.0
+bounce = 1
+
+while bounce <= 10:
+    height = height * (3/5)
+    print(str(bounce) + '\t' + str(round(height,4)))
+    bounce += 1
